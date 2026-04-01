db4fd0bb6890fcb3edd3dc4ef93ff15ad4c0cd7d
junghwan <vww2227@gmail.com>
2026-04-01 12:46:46 +0900

    모바일 수정 제발

--- script.js
+++ script.js
@@ -177,3 +177,16 @@ document.querySelectorAll(".nav-menu a").forEach(link=>{
   });
 });
 
+document.addEventListener("DOMContentLoaded", function(){
+
+  const toggle = document.querySelector(".menu-toggle");
+  const menu = document.querySelector(".navbar ul");
+
+  if(toggle && menu){
+    toggle.addEventListener("click", function(){
+      menu.classList.toggle("active");
+    });
+  }
+
+});
+
